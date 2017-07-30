2221439c83cfa667d9d1207b52436b4f5cc1d10c
Zeqi Cui <cuizeqiw@gmail.com>
2017-07-30 17:49:39 -0400

    new design

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php
+    $to = "zeqicui@live.com"; // ENTER EMAIL HERE
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+    $subject = "You have a message.";
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"message"} = "message";
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    $send = mail($to, $subject, $body, $headers);
+?>
